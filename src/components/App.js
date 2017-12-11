68fac87f3e764a4802c9871e489ff3e700b2769b
Denisse Suarez <dsuarez@thoughtworks.com>
2017-12-10 23:43:08 -0500

    Using Compound Components pattern to create a label that indicates the state of the button

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,21 @@ import '../styles/App.css';
 import Toggle from "./Toggle";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Toggle onToggle={on => console.log(on)} />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <Toggle onToggle={on => console.log(on)}>
+                    <Toggle.On>The button is on</Toggle.On>
+                    <Toggle.Off>The button is off</Toggle.Off>
+                    <Toggle.Button />
+                </Toggle>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,9 +1,26 @@
-//how to create a simple toggle with React setState
+//Compound component gives more rendering control to the user. The functionality of the component stays intact while how it looks and the order of the children can be changed at will. We get this functionality by using the special React.Children.map function to map over the children given to our <Toggle/> component. We map over the children to pass the on state as a prop to its children. We move the visual pieces of the component out into function components and add them as static properties to <Toggle/>.
 
-import React, { Component } from 'react';
+
+import React, { Component, Children } from 'react';
 import Switch from "./Switch";
 
+
+function ToggleOn ({on, children}) {
+    return on ? children : null;
+}
+function ToggleOff ({on, children}) {
+    return on ? null : children;
+}
+
+function ToggleButton ({on, toggle, ...props}) {
+    return <Switch on={on} onClick={toggle} {...props} />
+}
+
 class Toggle extends Component {
+    static On = ToggleOn;
+    static Off = ToggleOff;
+    static Button = ToggleButton;
+
     static defaultProps = {
         onToggle: () => {}
     };
@@ -14,11 +31,17 @@ class Toggle extends Component {
             ({on}) => ({on: !on}),
             () => {this.props.onToggle(this.state.on)},
             );
+
     render() {
-        const {on} = this.state;
-        return (
-            <Switch on={on} onClick={this.toggle} />
+        const children = Children.map(
+            this.props.children,
+            child =>
+                React.cloneElement(child, {
+                    on: this.state.on,
+                    toggle: this.toggle,
+                })
         );
+        return <div>{children}</div>;
     }
 }
 export default Toggle;
